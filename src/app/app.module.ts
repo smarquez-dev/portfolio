713e6b3f104f9eb4f5aed591fb7b14e2b4c1d1ed
Sergio Márquez Vázquez <smarquez.dev@gmail.com>
2017-12-11 17:03:31 +0100

    Header, body and footer ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html'
+})
+export class PortfolioComponent {
+
+
+}
